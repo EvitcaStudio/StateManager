eed2e4a920bd16ae2d19f311d1121e9297db13dd
Active <56242467+doubleactii@users.noreply.github.com>
2023-03-13 23:50:06 -0500

    Initial commit.

---
+++ src/state-manager.js
@@ -0,0 +1,118 @@
+/**
+ * A class representing a state machine manager.
+ * 
+ * @class StateManager  
+ * @license StateManager does not have a license at this time. For licensing contact the author
+ * @author https://github.com/doubleactii
+ * Copyright (c) 2023 Evitca Studio
+ */
+class StateManager {
+	/**
+	 * Creates a new state machine manager.
+	 * 
+	 * @param {Object<State>} pStates - Array of state objects to register.
+	 */
+	constructor(pStates) {
+		/**
+		 * An object containing all the registered states.
+		 * 
+		 * @type {Object<string, State>}
+		 */
+		this.states = {};
+
+		/**
+		 * The current state of the state machine.
+		 * 
+		 * @type {State|null}
+		 */
+		this.currentState = null;
+
+		// Loop passed states and register them. {'name': foo, 'state': state}
+		if (typeof (pStates) === 'object' && !Array.isArray(pStates)) {
+			for (const state in pStates) {
+				this.registerState();
+			}
+		}
+	}
+
+	/**
+	 * Registers a new state with the state machine.
+	 * 
+	 * @param {string} pName - The name of the state.
+	 * @param {State} pState - The state object.
+	 */
+	registerState(pName, pState) {
+		if (typeof (pName) === 'string') {
+			if (pState instanceof State) {
+				pState.name = pName;
+				this.states[pName] = pState;
+			} else {
+				console.error(`${pState} is not a valid state.`);
+			}
+		} else {
+			console.error('Invalid name for state.');
+		}
+	}
+
+	/**
+	 * Sets the current state of the state machine.
+	 * 
+	 * @param {string} pName - The name of the state to set.
+	 */
+	setState(pName) {
+		const state = this.states[pName];
+		if (!state) {
+			console.warn(`State ${pName} not found.`);
+			return;
+		}
+
+		if (this.currentState) {
+			this.currentState.exit();
+		}
+
+		this.currentState = state;
+		this.currentState.enter();
+	}
+
+	/**
+	 * Gets the current state.
+	 */
+	getCurrentState() {
+		return this.currentState.name;
+	}
+
+	/**
+	 * Updates the current state of the state machine.
+	 * 
+	 * @param {number} pDelta - The delta time since the last update in seconds.
+	 */
+	update(pDelta) {
+		if (this.currentState) {
+			this.currentState.update(pDelta);
+		}
+	}
+}
+
+/**
+ * A class representing a state.
+ */
+class State {
+	/**
+	 * Called when entering the state.
+	 */
+	enter() { }
+
+	/**
+	 * Called when executing the state.
+	 * 
+	 * @param {number} pDelta - The delta time since the last update in seconds.
+	 */
+	update(pDelta) { }
+
+	/**
+	 * Called when exiting the state.
+	 */
+	exit() { }
+}
+
+export { StateManager, State };
